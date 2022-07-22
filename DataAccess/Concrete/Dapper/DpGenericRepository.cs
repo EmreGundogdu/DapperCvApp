2bdc8093c202fa9f4ca70d86aefcea768f1f31c5
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-07-22 19:11:32 +0300

    Generic Repository added

---
+++ DataAccess/Concrete/Dapper/DpGenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper.Contrib.Extensions;
+using DataAccess.Interfaces;
+using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Dapper
+{
+    public class DpGenericRepository<T> : IGenericRepository<T> where T : class, ITable, new()
+    {
+        private readonly IDbConnection _connection;
+
+        public DpGenericRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Delete(T entity)
+        {
+            _connection.Delete(entity);
+        }
+
+        public List<T> GetAll()
+        {
+            return _connection.GetAll<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _connection.Get<T>(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _connection.Insert(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _connection.Update(entity);
+        }
+    }
+}
---
+++ DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IGenericRepository<T> where T : class, ITable, new()
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
