01f6052d9135c7e09715e3bea9583e620cbe395e
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-07-22 20:28:05 +0300

    added dtos, generic repository, business layer

---
+++ Business/Concrete/GenericManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Interfaces;
+using DataAccess.Interfaces;
+using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class GenericManager<T> : IGenericService<T> where T : class, ITable, new()
+    {
+        private readonly IGenericRepository<T> _genericRepository;
+
+        public GenericManager(IGenericRepository<T> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public void Delete(T entity)
+        {
+            _genericRepository.Delete(entity);
+        }
+
+        public List<T> GetAll()
+        {
+            return _genericRepository.GetAll();
+        }
+
+        public T GetById(int id)
+        {
+            return _genericRepository.GetById(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _genericRepository.Insert(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _genericRepository.Update(entity);
+        }
+    }
+}
---
+++ Business/Interfaces/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IGenericService<T> where T : class, ITable, new()
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
