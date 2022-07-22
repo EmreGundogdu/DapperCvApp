1e00cc126eb79831e6981efcabf59bd61fc4b1ad
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-07-22 20:33:51 +0300

    added validators

---
+++ Business/ValidationRules/AppUserUpdateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using DTO.DTOs.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class AppUserUpdateDtoValidator : AbstractValidator<AppUserUpdateDto>
+    {
+        public AppUserUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id boş geçilemez");
+        }
+    }
+}
---
+++ Business/ValidationRules/CertificationUpdateDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using DTO.DTOs.CertificationDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class CertificationUpdateDtoValidator : AbstractValidator<CertificationUpdateDto>
+    {
+        public CertificationUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id alanı boş geçilemez");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Sertifika alanı boş geçilemez");
+        }
+    }
+}
